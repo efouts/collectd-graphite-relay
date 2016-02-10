23ae98c3c2961192f36b5d8cfcb33dd7232e67c6
Elliott Fouts <efouts@qualtrax.com>
2016-02-10 12:10:10 -0500

    Fix typo on command line switch

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ sed -i -e "s/{COLLECTD_LISTEN_PORT}/$COLLECTD_LISTEN_PORT/g" ./etc/collectd/coll
 sed -i -e "s/{GRAPHITE_HOST}/$WRITE_GRAPHITE_HOST/g" ./etc/collectd/collectd.conf
 sed -i -e "s/{GRAPHITE_PORT}/$WRITE_GRAPHITE_PORT/g" ./etc/collectd/collectd.conf
 
-/usr/sbin/collectd -c /etc/collectd/collectd.conf -f
+/usr/sbin/collectd -C /etc/collectd/collectd.conf -f
